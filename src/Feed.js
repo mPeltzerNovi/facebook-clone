70e5924edc98afdd14968bf5cdee4a865c95a7bc
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-03-29 17:19:45 +0200

    Working render from firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
+import db from "./firebase";
 
 function Feed() {
+    //Hier haal je de boel eruit
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot((snapshot) =>
+                setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+        );
+    }, []);
+    /////////////////////////////////////
+
     return (
         <div className='feed'>
             <StoryReel />
             <MessageSender />
 
-            <Post
-                profilePic="https://cdna.lystit.com/photos/2012/05/05/hm-brown-bikini-bottoms-product-1-3419414-995708017.jpeg"
-                message="Het moet een beetje leuk blijven hoor!"
-                timestamp="This is a timestamp"
-                username="Emma van M"
-                image="https://oya.com/wp-content/uploads/2018/08/32-melges.jpg"
-            />
-            <Post
-                profilePic="https://cdna.lystit.com/photos/2012/08/28/hm-blue-bikini-bottoms-product-3-4595293-229457281.jpeg"
-                message="Het moet een beetje leuk blijven hoor!"
-                timestamp="Mooi dit"
-                username="Marjolein S"
-                image="https://www.chalet-pillerseetal.com/images/ski/skifahren-pillerseetal2.jpg"
-            />
-
-            <Post />
-
+            {posts.map((post) => (
+                <Post
+                    key={post.data.id}
+                    profilePic={post.data.profilePic}
+                    message={post.data.message}
+                    timestamp={post.data.timestamp}
+                    username={post.data.username}
+                    image={post.data.image}
+                />
+            ))}
         </div>
-    )
+    );
 }
 
 export default Feed;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAqgEqfF5sz4McwUmBKG7oJqdw347ylBSU",
     authDomain: "additional-database.firebaseapp.com",
@@ -7,3 +10,16 @@ const firebaseConfig = {
     appId: "1:839418379434:web:dd585f82ed94337de1e4d3",
     measurementId: "G-ZQW86K8CCG"
 };
+
+//connect react frondend to firebase backend:
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//getting acces to the database:
+const db = firebaseApp.firestore();
+
+//auth deel even mee-tikken, niet gebruiken:
+//const auth = firebase.auth();
+//const provider = new firebase.auth.GoogleAuthProvider();
+
+//export { auth, provider };
+export default db;
